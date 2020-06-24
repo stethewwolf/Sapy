d240405a7b46321672a95b2bfdafaf09ba484e7d
stethewwolf <stethewwolf@gmail.com>
2020-06-24 10:02:06 +0200

    closed #39

--- sapy_modules/gui/gtk/dialogs/update_mom_dialog.py
+++ sapy_modules/gui/gtk/dialogs/update_mom_dialog.py
@@ -18,10 +18,11 @@ import gi, datetime
 gi.require_version('Gtk', '3.0')
 from gi.repository import Gtk
 from sapy_modules.sapy import moms
+from calendar import monthrange
 
 class Update_Mom_Dialog_View(Gtk.MessageDialog):
     def __init__(self, parent, mom):
-        Gtk.Dialog.__init__(self, "add new mom", parent, 0,
+        Gtk.Dialog.__init__(self, "update new movement", parent, 0,
             (Gtk.STOCK_CANCEL, Gtk.ResponseType.CANCEL,
              Gtk.STOCK_OK, Gtk.ResponseType.OK))
         self.set_default_size(150, 100)
@@ -65,8 +66,6 @@ class Update_Mom_Dialog_View(Gtk.MessageDialog):
                 )
         
         date_label = Gtk.Label("Date")
-        self.date_entry = Gtk.Entry()
-        self.date_entry.set_placeholder_text(str(self.update_mom.time))
 
         grid.attach_next_to(
                 date_label, 
@@ -75,11 +74,60 @@ class Update_Mom_Dialog_View(Gtk.MessageDialog):
                 1, 1
                 )
 
-        grid.attach_next_to( 
-                self.date_entry,
+        year_label = Gtk.Label("Year")
+        year_adjustment = Gtk.Adjustment(mom.time.year, 1900, 2100, 1, 1, 0)
+        self.year_spin = Gtk.SpinButton()
+        self.year_spin.configure(year_adjustment,1,0)
+        self.year_spin.connect("value-changed",self.update_day_adj)
+
+        grid.attach_next_to(
+                year_label,
                 date_label, 
+                Gtk.PositionType.BOTTOM, 
+                1, 1
+                )
+        grid.attach_next_to(
+                self.year_spin,
+                year_label,
                 Gtk.PositionType.RIGHT, 
-                2, 1
+                1, 1
+                )
+
+        month_label = Gtk.Label("Month")
+        month_adjustment = Gtk.Adjustment(mom.time.month, 1, 12, 1, 1, 0)
+        self.month_spin = Gtk.SpinButton()
+        self.month_spin.configure(month_adjustment,1,0)
+        self.month_spin.connect("value-changed",self.update_day_adj)
+
+        grid.attach_next_to(
+                month_label,
+                year_label,
+                Gtk.PositionType.BOTTOM, 
+                1, 1
+                )
+        grid.attach_next_to(
+                self.month_spin,
+                month_label,
+                Gtk.PositionType.RIGHT, 
+                1, 1
+                )
+
+        day_label = Gtk.Label("Day")
+        day_adjustment = Gtk.Adjustment(mom.time.day, 1, monthrange(mom.time.year,mom.time.month)[1], 1, 1, 0)
+        self.day_spin = Gtk.SpinButton()
+        self.day_spin.configure(day_adjustment,1,0)
+
+        grid.attach_next_to(
+                day_label,
+                month_label,
+                Gtk.PositionType.BOTTOM, 
+                1, 1
+                )
+        grid.attach_next_to(
+                self.day_spin,
+                day_label,
+                Gtk.PositionType.RIGHT, 
+                1, 1
                 )
  
         box.add(grid)
@@ -88,14 +136,22 @@ class Update_Mom_Dialog_View(Gtk.MessageDialog):
     def run_update_mom(self):
         raw_cause =  self.cause_entry.get_text()
         raw_value = float(self.value_button.get_value())
+        raw_year = self.year_spin.get_value_as_int()
+        raw_month = self.month_spin.get_value_as_int()
+        raw_day = self.day_spin.get_value_as_int()
 
-        if len(self.date_entry.get_text().split("/")) == 3:
-            raw_year = self.date_entry.get_text().split("/")[2]
-            raw_month = self.date_entry.get_text().split("/")[1]
-            raw_day = self.date_entry.get_text().split("/")[0]
-            self.update_mom.update(new_value=raw_value, new_cause=raw_cause, new_year=raw_year, new_month=raw_month, new_day=raw_day)
-        else:
-            self.update_mom.update(new_value=raw_value, new_cause=raw_cause)
+        self.update_mom.update(new_value=raw_value, new_cause=raw_cause, new_year=raw_year, new_month=raw_month, new_day=raw_day)
+
+    def update_day_adj(self, widget):
+        year = self.year_spin.get_value_as_int()
+        month = self.month_spin.get_value_as_int()
+        max_days = monthrange(year,month)[1]
+
+        if self.day_spin.get_value_as_int() > max_days:
+            self.day_spin.set_value(max_days)
+        
+        day_adjustment = Gtk.Adjustment(self.day_spin.get_value_as_int(), 1, max_days, 1, 1, 0)
+        self.day_spin.configure(day_adjustment,1,0)
 
 
 class Update_Mom_Dialog_Controller(object):
