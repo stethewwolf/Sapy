df7a81d30e46872f7979a72f875b2a91d4505ca3
Stefnao Prina <stethewwolf@posteo.net>
2023-04-01 11:09:31 +0200

    updated balance

--- sapy/commands/runs/run_balance.py
+++ sapy/commands/runs/run_balance.py
@@ -15,33 +15,41 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 
-from sapy.utils import loggers as LoggerFactory
+from sapy.utils import loggers, values, printers
 from sapy.commands.command import Command
-import datetime
-import sapy.core.loms as loms
+from sapy.commands.setters import set_lom
+from sapy.core import profiles, loms
 
 
 class RunBalance (Command):
     short_arg = "b"
     long_arg = "balance"
-    cmd_help = "print the actual balance of the list"
+    cmd_help = "print the actual balance"
     cmd_type = None
     cmd_action = "store_true"
 
     def __init__(self, param):
         super().__init__()
-        self.logger = LoggerFactory.getLogger(str(self.__class__))
+        self.logger = loggers.getLogger(str(self.__class__))
 
     def run(self):
-        self.logger.debug("start")
+        profile_id = profiles.get_default_profile_id()
+        lom = loms.get_lom(name='Occurred')
+        start_date = None
+        end_date = None
+        
+        if values.has_value(set_lom.__lom_tag__):
+            lom = values.get_value(set_lom.__lom_tag__)
 
-        lom = self.values.get_value('lom')
+        profile = profiles.get_profile(id=profile_id)
 
-        ed = self.values.get_value('end_date')
-    
-        print('------------------------------')
-        print(lom.name)
-        print('------------------------------')
-        print(' balance : ' + str(lom.balance(end_date=ed)))
-        print('------------------------------')
-        self.logger.debug("end")
+        balance = []
+
+        if lom.name == 'Occurred':
+            balance = [ profile.name, 'Occurred', start_date, end_date, profile.get_occurred_balance(start_date, end_date) ]
+
+        if lom.name == 'Planned':
+            balance = [ profile.name, 'Planned', start_date, end_date, profile.get_planned_balance(start_date, end_date) ]
+
+        #printers.print_list('balance', balance)
+        printers.print_table(['Profile', 'Lom', 'Start Date', 'End Date', 'Balance'], balance)
